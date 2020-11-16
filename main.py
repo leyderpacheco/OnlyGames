f94ba9cd17be78cd7056708eca26905a7dbbd260
Leyder Pacheco <leyderj.j@gmail.com>
2020-11-16 00:40:58 -0500

    funcionamiento scrapper y templates

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
-def home():
+def log():
     return render_template('indexinicial.html')
 
 
-@app.route('/about')
-def about():
-    return 'about page'
+@app.route('/registro')
+def reg():
+    return render_template('indexreg.html')
+
+@app.route('/home')
+def home():
+    return render_template('menup.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrappy.py
+++ scrappy.py
@@ -1,5 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://store.steampowered.com/')
-soup = BeautifulSoup(r.text, 'lxml')
+URL = 'https://store.steampowered.com/app/933110/Age_of_Empires_III_Definitive_Edition/?curator_clanid=35480745'
+
+headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'}
+
+page = requests.get(URL,headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+titulo = soup.find("div",{"class":"apphub_AppName"}).get_text()
+precio = soup.find("div",{"class":"game_purchase_price price"}).get_text()
+
+
+print(titulo)
+print(precio)
