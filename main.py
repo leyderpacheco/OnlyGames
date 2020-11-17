71634c1a2fe911ac810d9f9fd2a62c8e64994d09
Leyder Pacheco <leyderj.j@gmail.com>
2020-11-16 22:53:19 -0500

    registro, sesiones y log out funcionando

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect,session
 import mysql.connector
+import os
 
 app=Flask(__name__)
+app.secret_key=os.urandom(24)
 
 cnx=mysql.connector.connect(user='root', password = 'Millos14', database='onlygames', host='127.0.0.1',auth_plugin='mysql_native_password')
 cursor=cnx.cursor()
@@ -18,25 +20,48 @@ def reg():
 
 @app.route('/home')
 def home():
-    return render_template('menup.html')
+    if 'user_id' in session:
+        return render_template('menup.html')
+    else:
+        return redirect('/')
 
 @app.route('/login_validation', methods=['POST'])
 def login_validation():
     user=request.form.get('usuario')
     password=request.form.get('contraseña')
 
-    query = ("SELECT * FROM usuarios where username= %s and contraseña = %s")
+    query = ("SELECT * FROM usuarios where username= %s and contraseña = SHA(%s)")
     data_usr = (user, password)
     cursor.execute(query, data_usr)
     users=cursor.fetchall()
 
     if len(users)>0:
-        return render_template('menup.html')
+        session['user_id']=users[0][0]
+        return redirect('/home')
     else:
-        return render_template('indexinicial.html')
+        return redirect('/')
 
 @app.route('/crear_usuario', methods=['POST'])
 def crear_usuario():
-    
+    user=request.form.get('usuario')
+    mail=request.form.get('correo')
+    password=request.form.get('contraseña')
+
+    add_usr = ("INSERT INTO usuarios "
+               "(username, correo, contraseña) "
+               "VALUES (%s, %s, SHA(%s))")
+
+
+    data_usr = (user,mail, password)
+    cursor.execute(add_usr, data_usr)
+    cnx.commit()
+
+    return "usuario registrado"
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id')
+    return redirect('/')
+
 if __name__ == "__main__":
     app.run(debug=True)
